7fce1692530d83709ada3b4f9c8faf7ce570cf02
ds4ouj@naver.com <ds4ouj@naver.com>
2021-07-26 17:10:29 +0900

    fs모듈을 통한 회원가입 기능

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,16 +1,25 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
     // 클래스 자체의 변수에 접근하기 위해서 static으로 설정
     // #는 users라는 내부데이터에 접근하는 것을 비공개로 만들기위해 사용 -> 은닉화
-    static #users = {
-        id: ["woorimIT", "나개발", "김팀장"],
-        psword: ["1234", "1234", "123456"],
-        name: ["우리밋", "나개발", "김팀장"],
-    };
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); // => [id, psword, name]
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        console.log(userInfo);
+        return userInfo;
+    }
 
-    static getUsers(...fields) {
-        const users = this.#users;
+    static #getUsers(data, isAll, fields) {
+        const users = JSON.parse(data);
+        if(isAll) return users;
         const newUsers = fields.reduce((newUsers, field) => {
             if(users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -20,23 +29,32 @@ class UserStorage {
         return newUsers;
     }
 
-    static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // => [id, psword, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+    static getUsers(isAll, ...fields) {
+        return fs.readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUsers(data, isAll, fields);
+            })
+            .catch(console.error);
     }
 
-    static save(userInfo) {
-        const users = this.#users;
+    static getUserInfo(id) {
+        return fs.readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);
+    }
+
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if(users.id.includes(userInfo.id)) {
+            throw "이미 존재하는 아이디입니다.";
+        }
+        //데이터 추가
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
         return { success: true };
     }
 }
