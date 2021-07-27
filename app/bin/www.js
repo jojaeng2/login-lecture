7d3c74663d4a6038f044cf508dd1d6e9a670a57c
ds4ouj@naver.com <ds4ouj@naver.com>
2021-07-27 11:09:30 +0900

    winston 적용

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const app = require("../app.js");
+const logger = require("../src/config/logger");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("서버 가동");
+    logger.info(`${PORT} 서버 가동`);
 });
